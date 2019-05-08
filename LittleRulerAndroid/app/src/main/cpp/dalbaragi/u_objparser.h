84af8f73f127fa9db80cc8f7de3c4886e2eaa02c
Sungmin Woo <woos8899@gmail.com>
2019-05-08 22:40:49 +0900

    It is sad to delete my own obj parser but it is not need after all. T_T

--- LittleRulerAndroid/app/src/main/cpp/dalbaragi/u_objparser.h
+++ LittleRulerAndroid/app/src/main/cpp/dalbaragi/u_objparser.h
@@ -1,51 +1,6 @@
 #pragma once
 
-#include <unordered_map>
-#include <vector>
-#include <string>
-#include <list>
-
-#include <glm/glm.hpp>
-
 #include "u_loadinfo.h"
-
-
-namespace dal_deprecated {
-
-	struct OBJObjectInfo_deprecated {
-		std::vector<float> mVertices, mTexcoords, mNormals;
-		std::vector<glm::vec3> debugVertices, debugNormals;
-		std::vector<glm::vec2> debugTexcoords;
-		std::string mName, mMaterialName;
-
-		OBJObjectInfo_deprecated(const char* const name) : mName(name) {
-
-		}
-	};
-
-	struct OBJInfo_deprecated {
-		std::vector<OBJObjectInfo_deprecated> mObjects;
-		std::vector<std::string> mMaterialFiles;
-	};
-
-	struct MTLMaterialInfo_deprecated {
-		glm::vec3 mDiffuseColor;
-		std::string mDiffuseMap, m_specularMap, mName;
-
-		MTLMaterialInfo_deprecated(const char* const name) : mName(name) {
-
-		}
-	};
-
-	struct MTLInfo_deprecated {
-		std::vector<MTLMaterialInfo_deprecated> mMateirals;
-	};
-
-
-	bool parseOBJ_deprecated(OBJInfo_deprecated* const con, const uint8_t* const buf, const size_t bufSize);
-
-	bool parseMTL_deprecated(MTLInfo_deprecated* info, const uint8_t* const buf, const size_t bufSize);
-}
 
 
 namespace dal {
