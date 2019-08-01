567055e7cbde9af336afc08cd41018ee7c2ac4e0
SausageTaste <woos8899@gmail.com>
2019-08-01 20:07:00 +0900

    Removed unused class dal::AABB_2D, and some chanegs.

--- LittleRulerAndroid/app/src/main/cpp/dalbaragi/m_collider.cpp
+++ LittleRulerAndroid/app/src/main/cpp/dalbaragi/m_collider.cpp
@@ -10,11 +10,13 @@ using namespace fmt::literals;
 
 namespace {
 
-    size_t minIndex(const float* const arr, const size_t arrSize) {
-        if ( arrSize == 0 ) {
+    size_t minValueIndex(const float* const arr, const size_t arrSize) {
+        if ( 0 == arrSize ) {
             dalAbort("Are you mad?");
         }
-        else if ( 1 == arrSize ) return 0;
+        else if ( 1 == arrSize ) {
+            return 0;
+        }
         else {
             float minValue = arr[0];
             size_t minIndex = 0;
@@ -35,35 +37,6 @@ namespace {
 
 namespace dal {
 
-    void AABB_2D::setPoints(const glm::vec2& p1, const glm::vec2& p2) {
-        this->p1 = p1;
-        this->p2 = p2;
-        this->validateOrder();
-    }
-
-    bool AABB_2D::isInside(const glm::vec2& p) const {
-        if ( p.x < p1.x ) return false;
-        if ( p.x > p2.x ) return false;
-        if ( p.y < p1.y ) return false;
-        if ( p.y > p2.y ) return false;
-
-        return true;
-    }
-
-    void AABB_2D::validateOrder(void) {
-        if ( p1.x > p2.x ) {
-            std::swap(p1.x, p2.x);
-        }
-        if ( p1.y > p2.y ) {
-            std::swap(p1.y, p2.y);
-        }
-    }
-
-}
-
-
-namespace dal {
-
     AxisAlignedBoundingBox::AxisAlignedBoundingBox(const glm::vec3& p1, const glm::vec3& p2, const float massInv)
         : m_p1(p1), m_p2(p2)
         , m_massInv(massInv)
@@ -74,6 +47,8 @@ namespace dal {
     void AxisAlignedBoundingBox::set(const glm::vec3& p1, const glm::vec3& p2) {
         this->m_p1 = p1;
         this->m_p2 = p2;
+
+        this->validateOrder();
     }
 
     void AxisAlignedBoundingBox::add(const glm::vec3& offset) {
@@ -111,6 +86,13 @@ namespace dal {
     }
 
     CollisionResolveInfo calcResolveInfo(const AxisAlignedBoundingBox& one, const AxisAlignedBoundingBox& other) {
+        const auto thisWeight = 1.0f / one.m_massInv;
+        const auto otherWeight = 1.0f / other.m_massInv;
+        const auto weightSum = thisWeight + otherWeight;
+        if ( 0.0f == weightSum ) {
+            return CollisionResolveInfo{ { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f } };
+        }
+
         const auto xOne = one.m_p2.x - other.m_p1.x;
         const auto xTwo = one.m_p1.x - other.m_p2.x;
         const auto xDistance = abs(xOne) < abs(xTwo) ? xOne : xTwo;
@@ -123,12 +105,6 @@ namespace dal {
         const auto zTwo = one.m_p1.z - other.m_p2.z;
         const auto zDistance = abs(zOne) < abs(zTwo) ? zOne : zTwo;
 
-        const auto thisWeight = 1.0f / one.m_massInv;
-        const auto otherWeight = 1.0f / other.m_massInv;
-        const auto weightSum = thisWeight + otherWeight;
-        if ( 0.0f == weightSum )
-            return CollisionResolveInfo{ { 0.0f, 0.0f, 0.0f },{ 0.0f, 0.0f, 0.0f } };
-
         const auto thisFactor = thisWeight / weightSum;
         const auto otherFactor = otherWeight / weightSum;
 
@@ -140,35 +116,20 @@ namespace dal {
         const auto yForOther = yDistance * otherFactor;
         const auto zForOther = zDistance * otherFactor;
 
-        const float selector[3] = { abs(xForThis), abs(yForThis), abs(zForThis) };
-        const auto index = minIndex(selector, 3);
+        const float selector[3] = { std::abs(xForThis), std::abs(yForThis), std::abs(zForThis) };
+        const auto index = minValueIndex(selector, 3);
         switch ( index ) {
 
         case 0:
-            return CollisionResolveInfo{ {xForThis, 0.0f, 0.0f}, {xForOther, 0.0f, 0.0f} };
+            return CollisionResolveInfo{ { xForThis, 0.0f, 0.0f }, { xForOther, 0.0f, 0.0f } };
         case 1:
-            return CollisionResolveInfo{ {0.0f, yForThis, 0.0f}, {0.0f, yForOther, 0.0f} };
+            return CollisionResolveInfo{ { 0.0f, yForThis, 0.0f }, { 0.0f, yForOther, 0.0f } };
         case 2:
-            return CollisionResolveInfo{ {0.0f, 0.0f, zForThis}, {0.0f, 0.0f, zForOther} };
+            return CollisionResolveInfo{ { 0.0f, 0.0f, zForThis }, { 0.0f, 0.0f, zForOther } };
         default:
             dalAbort("This can't happen!");
 
         }
-
-        /*
-        if ( abs(xForThis) <= abs(yForThis) && abs(xForThis) <= abs(zForThis) ) {
-            return CollisionResolveInfo{ {xForThis, 0.0f, 0.0f}, {xForOther, 0.0f, 0.0f} };
-        }
-        else if ( abs(yForThis) < abs(xForThis) && abs(yForThis) < abs(zForThis) ) {
-            return CollisionResolveInfo{ {0.0f, yForThis, 0.0f}, {0.0f, yForOther, 0.0f} };
-        }
-        else if ( abs(zForThis) < abs(xForThis) && abs(zForThis) < abs(yForThis) ) {
-            return CollisionResolveInfo{ {0.0f, 0.0f, zForThis}, {0.0f, 0.0f, zForOther} };
-        }
-        else {
-            dalAbort("WTF ({}, {}, {})"_format(xForThis, yForThis, zForThis));
-        }
-        */
     }
 
 }
--- LittleRulerAndroid/app/src/main/cpp/dalbaragi/m_collider.h
+++ LittleRulerAndroid/app/src/main/cpp/dalbaragi/m_collider.h
@@ -5,21 +5,6 @@
 
 namespace dal {
 
-    class AABB_2D {
-
-    private:
-        glm::vec2 p1, p2;
-
-    public:
-        void setPoints(const glm::vec2& p1, const glm::vec2& p2);
-        bool isInside(const glm::vec2& p) const;
-
-    private:
-        void validateOrder(void);
-
-    };
-
-
     struct CollisionResolveInfo {
         glm::vec3 m_this, m_other;
     };
