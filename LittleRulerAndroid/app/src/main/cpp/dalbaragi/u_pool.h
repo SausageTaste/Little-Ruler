0743dfe71702ba88d051258e82876bebd33e9be4
SausageTaste <woos8899@gmail.com>
2019-08-12 22:46:12 +0900

    StaticPool implemented using BlockAllocator.

--- LittleRulerAndroid/app/src/main/cpp/dalbaragi/u_pool.h
+++ LittleRulerAndroid/app/src/main/cpp/dalbaragi/u_pool.h
@@ -7,21 +7,21 @@
 
 namespace dal {
 
-    template <typename T, size_t SIZE>
+    template <size_t _BlockSize, size_t SIZE>
     class ObjectSizedBytesArray {
 
     private:
-        uint8_t m_arr[sizeof(T) * SIZE];
+        uint8_t m_arr[_BlockSize * SIZE];
 
     public:
-        T* operator[](const size_t index) {
-            const auto arrIndex = index * sizeof(T);
-            return reinterpret_cast<T*>(m_arr + arrIndex);
+        void* operator[](const size_t index) {
+            const auto arrIndex = index * _BlockSize;
+            return reinterpret_cast<void*>(m_arr + arrIndex);
         }
 
-        size_t calcIndexOf(const T* const p) const {
+        size_t calcIndexOf(void* const p) const {
             const auto cp = reinterpret_cast<const uint8_t*>(p);
-            return static_cast<size_t>(cp - &this->m_arr[0]) / sizeof(T);
+            return static_cast<size_t>(cp - &this->m_arr[0]) / _BlockSize;
         }
 
     };
@@ -31,17 +31,13 @@ namespace dal {
     class BlockAllocator {
 
     private:
-        ObjectSizedBytesArray<T, SIZE> m_pool;
+        ObjectSizedBytesArray<sizeof(T), SIZE> m_pool;
         bool m_allocFlags[SIZE] = { false };
 
         size_t m_nextToAlloc = 0;
         size_t m_allocSize = 0;
 
     public:
-        constexpr unsigned int getSize(void) {
-            return SIZE;
-        }
-
         T* alloc(void) {
             // This means pool is full. X(
             if ( this->m_nextToAlloc == SIZE ) {
@@ -51,11 +47,11 @@ namespace dal {
             const auto allocatedIndex = this->m_nextToAlloc++;
             this->m_allocFlags[allocatedIndex] = true;
             this->m_nextToAlloc = this->getFirstFreeIndex(allocatedIndex);
-            return this->m_pool[allocatedIndex];
+            return reinterpret_cast<T*>(this->m_pool[allocatedIndex]);
         }
 
-        void free(const T* const p) {
-            const auto freeIndex = this->m_pool.calcIndexOf(p);
+        void free(T* const p) {
+            const auto freeIndex = this->m_pool.calcIndexOf(reinterpret_cast<void*>(p));
             this->m_allocFlags[freeIndex] = false;
             this->m_nextToAlloc = freeIndex;
             this->m_allocSize--;
@@ -74,51 +70,27 @@ namespace dal {
     };
 
 
-    template <typename T, unsigned int SIZE>
+    template <typename T, size_t SIZE>
     class StaticPool {
 
     private:
-        ObjectSizedBytesArray<T, SIZE> m_pool;
-        bool m_allocFlags[SIZE] = { false };
-
-        unsigned int m_nextToAlloc = 0;
-        unsigned int m_allocSize = 0;
+        BlockAllocator<T, SIZE> m_allocator;
 
     public:
-        constexpr unsigned int getSize(void) {
-            return SIZE;
-        }
-
         template <typename... VAL_TYPE>
         T* alloc(VAL_TYPE&&... params) {
-            // This means pool is full. X(
-            if ( this->m_nextToAlloc == SIZE ) {
+            auto alloced = this->m_allocator.alloc();
+            if ( nullptr == alloced ) {
                 return nullptr;
             }
-
-            const auto allocatedIndex = this->m_nextToAlloc++;
-            this->m_allocFlags[allocatedIndex] = true;
-            this->m_nextToAlloc = this->getFirstFreeIndex(allocatedIndex);
-            T* pObj = this->m_pool[allocatedIndex];
-            return new (pObj) T(std::forward<VAL_TYPE>(params)...);
-        }
-
-        void free(const T* const p) {
-            (*p).~T();
-            const auto freeIndex = this->m_pool.calcIndexOf(p);
-            this->m_allocFlags[freeIndex] = false;
-            this->m_nextToAlloc = freeIndex;
-            this->m_allocSize--;
-        }
-
-    private:
-        unsigned int getFirstFreeIndex(const unsigned int startIndex = 0) {
-            for ( unsigned int i = startIndex; i < SIZE; i++ ) {
-                if ( !this->m_allocFlags[i] ) {
-                    return i;
-                }
+            else {
+                return new (alloced) T(std::forward<VAL_TYPE>(params)...);
             }
-            return SIZE;
+        }
+
+        void free(T* const p) {
+            (*p).~T();
+            this->m_allocator.free(p);
         }
 
     };
