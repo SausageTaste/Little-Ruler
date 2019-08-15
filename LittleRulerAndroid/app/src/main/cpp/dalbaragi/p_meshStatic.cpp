7b87170e7f8c1d295775ed031cc5658a460f0911
SausageTaste <woos8899@gmail.com>
2019-08-15 18:47:05 +0900

    Solved shadow mapping problem with confitional compiling.

--- LittleRulerAndroid/app/src/main/cpp/dalbaragi/p_meshStatic.cpp
+++ LittleRulerAndroid/app/src/main/cpp/dalbaragi/p_meshStatic.cpp
@@ -444,16 +444,18 @@ namespace dal {
 
         glBindTexture(GL_TEXTURE_2D, m_texID);
         {
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, width, height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT, nullptr);
+
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-            // Eunchae's computer cannot draw shadow with those lines.
-            //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
-            //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
-
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, width, height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT, nullptr);
+            // Nvidia cannot draw shadow with those lines, but Galaxy S7 needs it.
+#ifdef __ANDROID__
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+#endif
         } glBindTexture(GL_TEXTURE_2D, 0);
     }
 
