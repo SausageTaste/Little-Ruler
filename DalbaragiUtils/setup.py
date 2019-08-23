0ec990a2f22bcfe96eba31675f0d128dcb8deab7
Sungmin Woo <woos8899@gmail.com>
2019-08-23 14:58:46 +0900

    Imporved lib installer for python.

--- DalbaragiUtils/setup.py
+++ DalbaragiUtils/setup.py
@@ -1,39 +1,61 @@
 import os
-from typing import List, Tuple
+from typing import List
 from importlib.util import find_spec
 
 
-class LibInfo:
+class LibInstaller:
     def __init__(self, importName: str, installName: str):
         self.__importName = str(importName)
         self.__installName = str(installName)
 
-    def checkAvailable(self) -> bool:
+    def tryInstall(self) -> bool:
+        if self.__checkAvailable():
+            print("Library already exists: {}".format(self.__importName))
+            return True
+        else:
+            return self.__install()
+
+    def getImportName(self) -> str:
+        return self.__importName
+
+    def __checkAvailable(self) -> bool:
         spam_spec = find_spec(self.__importName)
         return spam_spec is not None
 
-    def install(self) -> None:
-        cmd = "pip install " + self.__installName
-        res = os.system(cmd)
-        if 0 != res:
-            print("Failed to install lib: {}, cmd: {}".format(self.__importName, cmd))
+    def __install(self) -> bool:
+        cmd = self.__makeCmdStr()
+        exitCode = os.system(cmd)
+        if 0 != exitCode:
+            print("Failed to install lib: {}, cmd: {}, exit code".format(self.__importName, cmd, exitCode))
+            return False
+        else:
+            return True
+
+    def __makeCmdStr(self) -> str:
+        return "pip install " + self.__installName
 
 
 def main():
-    res = os.system("python -m pip install --upgrade pip")
-    if 0 != res:
-        print("Failed to update pip.")
+    libs: List[LibInstaller] = [
+        LibInstaller("numpy", "numpy"),
+        LibInstaller("glm", "pyglm"),
+    ]
 
-    libs: List[LibInfo] = (
-        LibInfo("numpy", "numpy"),
-        LibInfo("glm", "pyglm"),
-    )
+    successCount: int = 0
+    failedList: List[str] = []
 
     for lib in libs:
-        if not lib.checkAvailable():
-            lib.install()
+        result = lib.tryInstall()
+        if result:
+            successCount += 1
+        else:
+            failedList.append(lib.getImportName())
 
-    input("All done successfully!")
+    print("=" * 40)
+    print("{} out of {} libs are available.".format(successCount, len(libs)))
+    if len(failedList):
+        print("Failed libs : {}".format(failedList))
+    input("Press a key to continue...")
 
 
 if __name__ == '__main__':
