ea20dbe7457a7cdec8f4ba3c7d0f8fcaead6e1d9
Sungmin Woo <woos8899@gmail.com>
2019-08-23 22:35:46 +0900

    Deprecating old map builders.

--- DalbaragiUtils/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def main():
-    os.system("dallevel compile intermediates/water_bowl.json intermediates/vast_citylike.json intermediates/broken.json")
-
-
-if __name__ == '__main__':
-    main()
