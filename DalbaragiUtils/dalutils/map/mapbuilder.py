87303729e5530000ae8f29e11221674ec3865a51
Sungmin Woo <woos8899@gmail.com>
2019-08-24 18:27:04 +0900

    Added metadata.

--- DalbaragiUtils/dalutils/map/mapbuilder.py
+++ DalbaragiUtils/dalutils/map/mapbuilder.py
@@ -3,17 +3,34 @@ import dalutils.map.primitives as pri
 import dalutils.map.objectdef as ode
 
 
+class MapMetadata(inf.IDataBlock):
+    def __init__(self):
+        self.__binVersion = pri.IntData(1)
+
+        super().__init__({
+            "bin_version": self.__binVersion,
+        })
+
+    def getBinary(self) -> bytearray:
+        data = bytearray()
+        data += self.__binVersion.getBinary()
+        return data
+
+
 class MapChunkBuilder(inf.IDataBlock):
     def __init__(self):
+        self.__metadata = MapMetadata()
         self.__modelEmbedded = pri.UniformList(ode.ModelEmbedded)
 
         super().__init__({
+            "metadata": self.__metadata,
             "model_embedded": self.__modelEmbedded,
         })
 
     def getBinary(self) -> bytearray:
         data = bytearray()
 
+        data += self.__metadata.getBinary()
         data += self.__modelEmbedded.getBinary()
 
         return data
