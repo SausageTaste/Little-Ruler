4db13c0f52ecdb4618a0977fdf7695284b068893
Sungmin Woo <woos8899@gmail.com>
2019-09-11 23:40:22 +0900

    Added skybox assets.

--- LittleRulerAndroid/app/src/main/cpp/dalbaragi/u_fileclass.cpp
+++ LittleRulerAndroid/app/src/main/cpp/dalbaragi/u_fileclass.cpp
@@ -57,6 +57,7 @@ namespace {
             { "texture", {
                     {"dva", {}},
                     {"yuri2", {}},
+                    {"skybox", {}},
                     {"mess", {}}
                 }
             }
