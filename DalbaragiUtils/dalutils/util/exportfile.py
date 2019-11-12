7ae9e57c70cb368eb4f8b0eabdc4e80f83067dfc
Sungmin Woo <woos8899@gmail.com>
2019-11-12 11:43:36 +0900

    Python project has been moved to its own repository.
    https://github.com/SausageTaste/Dalbaragi-Utils

--- DalbaragiUtils/dalutils/util/exportfile.py
+++
@@ -1,9 +0,0 @@
-import zlib
-
-import dalutils.util.binutil as but
-
-
-def compressWithSizeInt32(data: bytes) -> bytes:
-    zipData: bytes = zlib.compress(data, zlib.Z_BEST_COMPRESSION)
-    originalSize: bytes = but.get4BytesInt(len(data))
-    return originalSize + zipData
