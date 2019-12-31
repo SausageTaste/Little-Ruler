f3d1a506265c23f06b8e224e94b3f580a024cea5
Sungmin Woo <woos8899@gmail.com>
2019-12-31 17:08:05 +0900

    DataView_Actor improved.

--- LittleRulerWindows/DalbaragiMapper/d_property_view.cpp
+++ LittleRulerWindows/DalbaragiMapper/d_property_view.cpp
@@ -1,116 +1,11 @@
 #include "d_property_view.h"
 
-#include <iostream>
-#include <functional>
-
 #include <QLabel>
 #include <QLineEdit>
 #include <QGridLayout>
 #include <QDoubleSpinBox>
 
 #include <d_pool.h>
-#include <d_logger.h>
-
-
-namespace {
-
-    template <unsigned _Size>
-    class VectorSpinBox : public QWidget {
-
-    public:
-        using valueArray_t = std::array<double, _Size>;
-        using callbackOnValueChange_t = std::function<void(const valueArray_t&)>;
-
-    private:
-        static constexpr QSizePolicy SIZE_POLICY{ QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Fixed };
-
-        QGridLayout m_grid;
-        dal::NoInitArray<QDoubleSpinBox, _Size> m_spinboxes;
-        dal::NoInitArray<QLabel, _Size> m_labels;
-        callbackOnValueChange_t m_onValueChange;
-
-    public:
-        VectorSpinBox(QWidget* const parent)
-            : QWidget(parent)
-            , m_grid(this)
-        {
-            this->setLayout(&this->m_grid);
-            this->m_grid.setAlignment(Qt::AlignLeft);
-            this->m_grid.setMargin(0);
-
-            for ( unsigned i = 0; i < _Size; ++i ) {
-                {
-                    QLabel& w = this->m_labels.construct(i, this);
-                    this->m_grid.addWidget(&w, i, 0, 1, 1);
-                }
-
-                {
-                    QDoubleSpinBox& w = this->m_spinboxes.construct(i, this);
-                    this->m_grid.addWidget(&w, i, 1, 1, 1);
-                    const auto _ = connect(&w, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged), this, &VectorSpinBox::onValueChange);
-
-                    w.setMinimumWidth(80);
-                    w.setMaximumWidth(99999);
-                    w.setSizePolicy(SIZE_POLICY);
-
-                    w.setMinimum(-DBL_MAX);
-                    w.setMaximum(DBL_MAX);
-                    w.setSingleStep(0.01);
-                    w.setDecimals(3);
-                    w.setSuffix(" m");
-                }
-            }
-        }
-
-        template <unsigned _Index>
-        void setValue(const double v) {
-            static_assert(_Index < _Size);
-            QDoubleSpinBox& w = this->m_spinboxes[_Index];
-            w.setValue(v);
-        }
-        template <unsigned _Index>
-        double value(void) const {
-            static_assert(_Index < _Size);
-            QDoubleSpinBox& w = this->m_spinboxes[_Index];
-            return w.value();
-        }
-        template <unsigned _Index>
-        void setText(const char* const str) {
-            static_assert(_Index < _Size);
-            QLabel& w = this->m_labels[_Index];
-            w.setText(str);
-        }
-
-        void enable(void) {
-            for ( unsigned i = 0; i < _Size; ++i ) {
-                this->m_spinboxes[i].setDisabled(false);
-            }
-        }
-        void disable(void) {
-            for ( unsigned i = 0; i < _Size; ++i ) {
-                this->m_spinboxes[i].setDisabled(true);
-            }
-        }
-
-        void register_onValueChange(callbackOnValueChange_t func) {
-            this->m_onValueChange = func;
-        }
-
-    private:
-        void onValueChange(const double x) {
-            if ( this->m_onValueChange ) {
-                valueArray_t arg;
-                for ( unsigned i = 0; i < _Size; ++i ) {
-                    arg[i] = this->m_spinboxes[i].value();
-                }
-
-                this->m_onValueChange(arg);
-            }
-        }
-
-    };
-
-}
 
 
 namespace {
@@ -140,24 +35,22 @@ namespace {
         virtual void onSharedInfoUpdated(void) {}
 
     };
-
+    
 
     class DataView_Actor : public PropertyPage {
 
     private:
-        bool m_isConnectedSlotDisabled = false;
+        static constexpr unsigned NUM_DOUBLE_VALUE = 3 + 4 + 1;
+        static constexpr QSizePolicy LABEL_SIZE_POLICY{ QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Fixed };
+
+    private:
+        bool m_slotDisabled = false;
 
         QLabel m_label_name;
         QLineEdit m_lineEdit_name;
 
-        QLabel m_label_pos;
-        VectorSpinBox<3> m_field_pos;
-
-        QLabel m_label_quat;
-        VectorSpinBox<4> m_field_quat;
-
-        QLabel m_label_scale;
-        VectorSpinBox<1> m_field_scale;
+        dal::NoInitArray<QDoubleSpinBox, NUM_DOUBLE_VALUE> m_dspinboxes;
+        dal::NoInitArray<QLabel, NUM_DOUBLE_VALUE> m_labels_forDspinboxes;
 
     public:
         DataView_Actor(QWidget* const parent, dal::Scene& scene, dal::SharedInfo& shared)
@@ -165,91 +58,129 @@ namespace {
 
             , m_label_name(this)
             , m_lineEdit_name(this)
-            , m_label_pos(this)
-            , m_field_pos(this)
-            , m_label_quat(this)
-            , m_field_quat(this)
-            , m_label_scale(this)
-            , m_field_scale(this)
         {
             this->setLayout(&this->m_grid);
             this->m_grid.setAlignment(Qt::AlignTop);
 
             // Name
             {
-                this->m_grid.addWidget(&this->m_label_name, 0, 0, 1, 1);
+                this->m_grid.addWidget(&this->m_label_name, 0, 0, 1, 1, Qt::AlignRight);
                 this->m_label_name.setText("Name");
+                this->m_label_name.setSizePolicy(this->LABEL_SIZE_POLICY);
 
                 this->m_grid.addWidget(&this->m_lineEdit_name, 0, 1, 1, 1);
-                const auto _ = connect(&this->m_lineEdit_name,
-                    static_cast<void(QLineEdit::*)(const QString&)>(&QLineEdit::textChanged),
-                    this,
-                    &DataView_Actor::onNameEditted
+                const auto _ = connect(
+                    &this->m_lineEdit_name, static_cast<void(QLineEdit::*)(const QString&)>(&QLineEdit::textChanged),
+                    this, [this](const QString& str) {
+                        if ( this->m_slotDisabled )
+                            return;
+
+                        const auto actor = this->findActiveActor();
+                        if ( nullptr != actor ) {
+                            actor->m_name = str.toStdString();
+                        }
+                    }
                 );
             }
 
-            // Pos
+            // Construct pos(3), quat(4), scale(1)
             {
-                this->m_grid.addWidget(&this->m_label_pos, 1, 0, 1, 1);
-                this->m_label_pos.setText("Pos");
-                QSizePolicy policy;
-                policy.setHorizontalPolicy(QSizePolicy::Policy::Fixed);
-                this->m_label_pos.setSizePolicy(policy);
+                for ( unsigned i = 0; i < NUM_DOUBLE_VALUE; ++i ) {
+                    constexpr QSizePolicy sizePolicy{ QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Fixed };
 
-                this->m_grid.addWidget(&this->m_field_pos, 1, 1, 1, 1);
-                this->m_field_pos.register_onValueChange([this](const VectorSpinBox<3>::valueArray_t& arg) {
-                    if ( this->m_isConnectedSlotDisabled )
-                        return;
+                    auto& w = this->m_dspinboxes.construct(i, this);
+                    this->m_grid.addWidget(&w, i + 1, 1, 1, 1);
 
-                    auto actor = this->findActiveActor();
-                    if ( nullptr != actor ) {
-                        actor->m_trans.setPos(arg[0], arg[1], arg[2]);
-                        this->m_shared.m_needRedraw = true;
-                    }
-                    });
-                this->m_field_pos.setText<0>("x");
-                this->m_field_pos.setText<1>("y");
-                this->m_field_pos.setText<2>("z");
+                    w.setSizePolicy(sizePolicy);
+                    w.setMinimumWidth(80);
+                    w.setMaximumWidth(99999);
+
+                    w.setMinimum(-DBL_MAX);
+                    w.setMaximum(DBL_MAX);
+                    w.setSingleStep(0.01);
+                    w.setDecimals(3);
+                    w.setSuffix(" m");
+                }
+
+                for ( unsigned i = 0; i < NUM_DOUBLE_VALUE; ++i ) {
+                    auto& w = this->m_labels_forDspinboxes.construct(i, this);
+                    this->m_grid.addWidget(&w, i + 1, 0, 1, 1, Qt::AlignRight);
+                    w.setSizePolicy(this->LABEL_SIZE_POLICY);
+                }
             }
 
-            // Quat
             {
-                this->m_grid.addWidget(&this->m_label_quat, 2, 0, 1, 1);
-                this->m_label_quat.setText("Quat");
+                this->m_labels_forDspinboxes[0].setText("Pos  x");
+                this->m_labels_forDspinboxes[1].setText("y");
+                this->m_labels_forDspinboxes[2].setText("z");
 
-                this->m_grid.addWidget(&this->m_field_quat, 2, 1, 1, 1);
-                this->m_field_quat.register_onValueChange([this](const VectorSpinBox<4>::valueArray_t& arg) {
-                    if ( this->m_isConnectedSlotDisabled )
-                        return;
+                this->m_labels_forDspinboxes[3].setText("Quat  w");
+                this->m_labels_forDspinboxes[4].setText("x");
+                this->m_labels_forDspinboxes[5].setText("y");
+                this->m_labels_forDspinboxes[6].setText("z");
 
-                    auto actor = this->findActiveActor();
-                    if ( nullptr != actor ) {
-                        actor->m_trans.setQuat(arg[1], arg[2], arg[3], arg[0]);
-                        this->m_shared.m_needRedraw = true;
-                    }
-                    });
-                this->m_field_quat.setText<0>("w");
-                this->m_field_quat.setText<1>("x");
-                this->m_field_quat.setText<2>("y");
-                this->m_field_quat.setText<3>("z");
+                this->m_labels_forDspinboxes[7].setText("Scale  xyz");
             }
 
-            // Scale
+            // Connect
             {
-                this->m_grid.addWidget(&this->m_label_scale, 3, 0, 1, 1);
-                this->m_label_scale.setText("Scale");
+                const auto signalFunc = static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged);
 
-                this->m_grid.addWidget(&this->m_field_scale, 3, 1, 1, 1);
-                this->m_field_scale.register_onValueChange([this](const VectorSpinBox<1>::valueArray_t& arg) {
-                    if ( this->m_isConnectedSlotDisabled )
+                QMetaObject::Connection _;
+
+                auto posUpdateFunc = [this](const double v) {
+                    if ( this->m_slotDisabled )
                         return;
 
                     auto actor = this->findActiveActor();
                     if ( nullptr != actor ) {
-                        actor->m_trans.setScale(arg[0]);
+                        actor->m_trans.setPos(
+                            this->m_dspinboxes[0].value(),
+                            this->m_dspinboxes[1].value(),
+                            this->m_dspinboxes[2].value()
+                        );
                         this->m_shared.m_needRedraw = true;
                     }
-                    });
+                };
+
+                _ = connect(&this->m_dspinboxes[0], signalFunc, this, posUpdateFunc);
+                _ = connect(&this->m_dspinboxes[1], signalFunc, this, posUpdateFunc);
+                _ = connect(&this->m_dspinboxes[2], signalFunc, this, posUpdateFunc);
+
+                auto quatUpdateFunc = [this](const double v) {
+                    if ( this->m_slotDisabled )
+                        return;
+
+                    auto actor = this->findActiveActor();
+                    if ( nullptr != actor ) {
+                        actor->m_trans.setQuat(
+                            this->m_dspinboxes[4].value(),
+                            this->m_dspinboxes[5].value(),
+                            this->m_dspinboxes[6].value(),
+                            this->m_dspinboxes[3].value()
+                        );
+                        this->m_shared.m_needRedraw = true;
+                    }
+                };
+
+                _ = connect(&this->m_dspinboxes[3], signalFunc, this, quatUpdateFunc);
+                _ = connect(&this->m_dspinboxes[4], signalFunc, this, quatUpdateFunc);
+                _ = connect(&this->m_dspinboxes[5], signalFunc, this, quatUpdateFunc);
+                _ = connect(&this->m_dspinboxes[6], signalFunc, this, quatUpdateFunc);
+
+                _ = connect(
+                    &this->m_dspinboxes[7], signalFunc,
+                    this, [this](const double v) {
+                        if ( this->m_slotDisabled )
+                            return;
+
+                        auto actor = this->findActiveActor();
+                        if ( nullptr != actor ) {
+                            actor->m_trans.setScale(v);
+                            this->m_shared.m_needRedraw = true;
+                        }
+                    }
+                );
             }
         }
 
@@ -263,44 +194,40 @@ namespace {
             this->enable();
             auto& actor = *ptr;
 
-            this->m_isConnectedSlotDisabled = true;
+            this->m_slotDisabled = true;
             {
                 this->m_lineEdit_name.setText(actor.m_name.c_str());
 
-                this->m_field_pos.setValue<0>(actor.m_trans.pos().x);
-                this->m_field_pos.setValue<1>(actor.m_trans.pos().y);
-                this->m_field_pos.setValue<2>(actor.m_trans.pos().z);
+                this->m_dspinboxes[0].setValue(actor.m_trans.pos().x);
+                this->m_dspinboxes[1].setValue(actor.m_trans.pos().y);
+                this->m_dspinboxes[2].setValue(actor.m_trans.pos().z);
 
-                this->m_field_quat.setValue<0>(actor.m_trans.quat().w);
-                this->m_field_quat.setValue<1>(actor.m_trans.quat().x);
-                this->m_field_quat.setValue<2>(actor.m_trans.quat().y);
-                this->m_field_quat.setValue<3>(actor.m_trans.quat().z);
+                this->m_dspinboxes[3].setValue(actor.m_trans.quat().w);
+                this->m_dspinboxes[4].setValue(actor.m_trans.quat().x);
+                this->m_dspinboxes[5].setValue(actor.m_trans.quat().y);
+                this->m_dspinboxes[6].setValue(actor.m_trans.quat().z);
 
-                this->m_field_scale.setValue<0>(actor.m_trans.scale());
+                this->m_dspinboxes[7].setValue(actor.m_trans.scale());
             }
-            this->m_isConnectedSlotDisabled = false;
+            this->m_slotDisabled = false;
         }
 
     private:
-        void onNameEditted(const QString& str) {
-            const auto actor = this->findActiveActor();
-            if ( nullptr != actor ) {
-                actor->m_name = str.toStdString();
-                dalVerbose(actor->m_name);
-            }
-        }
-
         void enable(void) {
             this->m_lineEdit_name.setDisabled(false);
-            this->m_field_pos.enable();
-            this->m_field_quat.enable();
-            this->m_field_scale.enable();
+            
+            for ( unsigned i = 0; i < NUM_DOUBLE_VALUE; ++i ) {
+                auto& w = this->m_dspinboxes[i];
+                w.setDisabled(false);
+            }
         }
         void disable(void) {
             this->m_lineEdit_name.setDisabled(true);
-            this->m_field_pos.disable();
-            this->m_field_quat.disable();
-            this->m_field_scale.disable();
+
+            for ( unsigned i = 0; i < NUM_DOUBLE_VALUE; ++i ) {
+                auto& w = this->m_dspinboxes[i];
+                w.setDisabled(true);
+            }
         }
 
         dal::Actor* findActiveActor(void) const {
