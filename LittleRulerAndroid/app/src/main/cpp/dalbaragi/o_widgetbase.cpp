2fb5a9bc75c65c69a699588258d9436d3ebb6946
Sungmin Woo <woos8899@gmail.com>
2020-02-06 04:13:56 +0900

    Removed inline to fix compilation error upon building release apk.

--- LittleRulerAndroid/app/src/main/cpp/dalbaragi/o_widgetbase.cpp
+++ LittleRulerAndroid/app/src/main/cpp/dalbaragi/o_widgetbase.cpp
@@ -65,25 +65,25 @@ namespace {
 // Header functions
 namespace dal {
 
-    inline glm::vec2 screen2device(const glm::vec2& p, const float winWidth, const float winHeight) {
+    glm::vec2 screen2device(const glm::vec2& p, const float winWidth, const float winHeight) {
         return glm::vec2{
              2.0f * p.x / (winWidth) - 1.0f,
             -2.0f * p.y / (winHeight) + 1.0f
         };
     }
 
-    inline glm::vec2 screen2device(const glm::vec2& p, const unsigned int winWidth, const unsigned int winHeight) {
+    glm::vec2 screen2device(const glm::vec2& p, const unsigned int winWidth, const unsigned int winHeight) {
         return screen2device(p, static_cast<float>(winWidth), static_cast<float>(winHeight));
     }
 
-    inline glm::vec2 device2screen(const glm::vec2& p, const float winWidth, const float winHeight) {
+    glm::vec2 device2screen(const glm::vec2& p, const float winWidth, const float winHeight) {
         return glm::vec2{
             (p.x + 1.0f) * (winWidth) / 2.0f,
             (1.0f - p.y) * (winHeight) / 2.0f
         };
     }
 
-    inline glm::vec2 device2screen(const glm::vec2& p, const unsigned int winWidth, const unsigned int winHeight) {
+    glm::vec2 device2screen(const glm::vec2& p, const unsigned int winWidth, const unsigned int winHeight) {
         return device2screen(p, static_cast<float>(winWidth), static_cast<float>(winHeight));
     }
 
