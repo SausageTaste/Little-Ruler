e7fcac9c93f9bdefa78d744e23ae942089fa0066
Sungmin Woo <woos8899@gmail.com>
2020-03-21 01:27:36 +0900

    Added data structures for map loader v2.

--- engine/Dalbaragi/resparser/d_mapdata.cpp
+++ engine/Dalbaragi/resparser/d_mapdata.cpp
@@ -1,1 +1,35 @@
 #include "d_mapdata.h"
+
+
+namespace dal::v2 {
+
+    size_t MeshRaw::numVertices(void) const {
+        assert(this->checkSizeValidity());
+        return this->m_uvcoords.size() / 2;
+    }
+
+    const float* MeshRaw::vertices(void) const {
+        assert(this->checkSizeValidity());
+        return this->m_vertices.data();
+    }
+
+    const float* MeshRaw::uvcoords(void) const {
+        assert(this->checkSizeValidity());
+        return this->m_uvcoords.data();
+    }
+
+    const float* MeshRaw::normals(void) const {
+        assert(this->checkSizeValidity());
+        return this->m_normals.data();
+    }
+
+    bool MeshRaw::checkSizeValidity(void) const {
+        if ( this->m_vertices.size() != this->m_normals.size() )
+            return false;
+        if ( 2 * this->m_vertices.size() != 3 * this->m_uvcoords.size() )
+            return false;
+
+        return true;
+    }
+
+}
--- engine/Dalbaragi/resparser/d_mapdata.h
+++ engine/Dalbaragi/resparser/d_mapdata.h
@@ -1,8 +1,10 @@
 #pragma once
 
-#include <cstdint>
+#include <memory>
 #include <string>
 #include <vector>
+#include <variant>
+#include <cstdint>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
@@ -65,6 +67,151 @@ namespace dal::v1 {
         std::vector<ModelImported> m_importedModels;
         std::vector<WaterPlane> m_waterPlanes;
         std::vector<PointLight> m_plights;
+    };
+
+}
+
+
+namespace dal::v2 {
+
+    // Render info
+
+    class Material {
+
+    public:
+        std::string m_albedoMap;
+        std::string m_roughnessMap;
+        std::string m_metallicMap;
+        float m_roughness = 0.5f;
+        float m_metallic = 1.f;
+
+    };
+
+    class IMesh {
+
+    public:
+        virtual ~IMesh(void) = default;
+
+        virtual size_t numVertices(void) const = 0;
+        virtual const float* vertices(void) const = 0;  // Size is 3 * numVertices();
+        virtual const float* uvcoords(void) const = 0;  // Size is 2 * numVertices();
+        virtual const float* normals(void) const = 0;   // Size is 3 * numVertices();
+
+    };
+
+    class MeshRaw : IMesh {
+
+    public:
+        std::vector<float> m_vertices, m_uvcoords, m_normals;
+
+    public:
+        virtual size_t numVertices(void) const override;
+        virtual const float* vertices(void) const override;
+        virtual const float* uvcoords(void) const override;
+        virtual const float* normals(void) const override;
+
+    private:
+        bool checkSizeValidity(void) const;
+
+    };
+
+    class RenderUnit {
+
+    public:
+        std::unique_ptr<IMesh> m_mesh;
+        Material m_material;
+
+    };
+
+    class RenderUnitRaw {
+
+    public:
+        MeshRaw m_mesh;
+        Material m_material;
+
+    };
+
+    class ModelEmbeded {
+
+    public:
+        std::string m_modelID;
+        std::vector<RenderUnit> m_renderUnits;
+
+    };
+
+    class ModelImported {
+
+        std::string m_resourceID;
+
+    };
+
+
+    // Actor
+
+    namespace cpnt {
+
+        class Transform {
+
+        public:
+            glm::vec3 m_pos{ 0 };
+            glm::quat m_quat;
+            float m_scale = 1.f;
+
+        };
+
+        class Model {
+
+        public:
+            std::string m_modelID;
+
+        };
+
+    }
+
+    using component_t = std::variant<
+        cpnt::Transform,
+        cpnt::Model
+    >;
+
+    class StaticModelActor {
+
+    public:
+        std::string m_name;
+        cpnt::Model m_modelName;
+        cpnt::Transform m_trans;
+
+    };
+
+    class DynamicActor {
+
+    public:
+        std::string m_name;
+        std::vector<component_t> m_components;
+
+    };
+
+
+    // Misc
+
+    class WaterPlane {
+
+    public:
+        glm::vec3 m_centerPos{ 0 };
+        glm::vec3 m_deepColor{ 0 };
+        float m_width = 1.f, height = 1.f;
+        float m_flowSpeed;
+        float m_waveStreng;
+        float m_darkestDepth;
+        float m_reflectance;
+
+    };
+
+    class PointLight {
+
+    public:
+        glm::vec3 m_pos{ 0 }, m_color{ 0 };
+        float m_maxDist = 5.f;
+
     };
 
 }
