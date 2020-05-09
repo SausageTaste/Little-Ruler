27c7f6f09bea026411a22dc091952f3f9cbac72e
Sungmin Woo <woos8899@gmail.com>
2020-05-09 20:52:09 +0900

    Widget2D added and more.

--- engine/Dalbaragi/overlay/d_widget.h
+++ engine/Dalbaragi/overlay/d_widget.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <d_input_data.h>
+#include <d_aabb_2d.h>
 
 
 namespace dal {
@@ -37,13 +38,30 @@ namespace dal {
 
         virtual void render(const float width, const float height, void* userData) {}
         virtual auto onTouch(const TouchEvent& e) -> InputDealtFlag {
-            return InputCtrlFlag::ignored;
+            return InputDealtFlag::ignored;
         }
         virtual auto onKeyInput(const KeyboardEvent& e, const KeyStatesRegistry& keyStates) -> InputDealtFlag {
-            return InputCtrlFlag::ignored;
+            return InputDealtFlag::ignored;
         }
         virtual void onParentResize(const float width, const float height) {}
         virtual void onFocusChange(const bool v) {}
+
+    };
+
+    class Widget2D : public Widget {
+
+    private:
+        AABB_2D<float> m_aabb;
+
+    public:
+        using Widget::Widget;
+
+        auto aabb(void) -> AABB_2D<float>& {
+            return this->m_aabb;
+        }
+        auto aabb(void) const -> const AABB_2D<float> {
+            return this->m_aabb;
+        }
 
     };
 
