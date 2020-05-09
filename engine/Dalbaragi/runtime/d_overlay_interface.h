69cf9a0c94eeda99555cce70d48f6736422e06d8
Sungmin Woo <woos8899@gmail.com>
2020-05-09 22:07:47 +0900

    Overlay interface.

---
+++ engine/Dalbaragi/runtime/d_overlay_interface.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <memory>
+
+#include <d_widget.h>
+
+#include "p_uniloc.h"
+#include "p_meshStatic.h"
+
+
+namespace dal {
+
+    class OverlayTexInterf : public OverlayTexture {
+
+    public:
+        std::shared_ptr<Texture> m_tex;
+
+    public:
+        virtual void init_maskMap(const uint8_t* const image, const unsigned width, const unsigned height) override;
+
+    };
+
+
+    void drawOverlay(const OverlayDrawInfo& info, const UniRender_Overlay* const uniloc);
+
+}
